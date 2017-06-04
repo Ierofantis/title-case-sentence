808c85115f4b2cc233f76afd2c034d0b3ce042e3
Theodore P <evil69669@gmail.com>
2017-06-04 14:32:59 +0300

    0.0.3

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 /* This program is free software. It comes without any warranty, to
      * the extent permitted by applicable law. You can redistribute it
      * and/or modify it under the terms of the Do What The Fuck You Want
-     */
+*/
 'use strict';
 module.exports = titleCase;
 
 function titleCase(str) {
-  var i;
- var newstr = str.split(" ");
-  
-  for(i=0;i<newstr.length;i++){
-    
+ var i;
+ var newstr = str.split(" ");  
+ 
+  for(i=0;i<newstr.length;i++){    
     var copy = newstr[i].substring(1).toLowerCase();
-    newstr[i] = newstr[i][0].toUpperCase() + copy;
-    
+    newstr[i] = newstr[i][0].toUpperCase() + copy;    
   }
    newstr = newstr.join(" ");
    return newstr;
